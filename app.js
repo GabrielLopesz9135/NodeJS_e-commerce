8d3d9077e907280a29c8843eafd02ce2166d4d96
Gabriel Lopes Ribeiro da Costa <116298645+GabrielLopesz9135@users.noreply.github.com>
2026-09-21 15:59:21 -0300

    feat: adiciona middleware de segurança e registro de acesso; atualiza dependências no package.json

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 require('dotenv').config();
 
 const express = require('express');
@@ -10,6 +11,9 @@ const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require("csurf");
 const flash = require('connect-flash');
 const multer = require('multer');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
 
 const errorController = require('./controllers/error');
 
@@ -27,6 +31,12 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth')
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+
+app.use(helmet());
+app.use(compression());
+app.use(morgan('combined', { stream: accessLogStream }));
 
 try{
   const fileStorage = multer.diskStorage({
@@ -110,7 +120,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(result => {
-    app.listen(3000);
+    app.listen(process.env.PORT || 3000);
   })
   .catch(err => {
     console.log(err);
